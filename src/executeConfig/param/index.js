15df07110e624fda584a1100ea39dbab6126ad9c
maosi <1601466212@qq.com>
2025-01-06 21:54:02 +0800

    feat: 支持不加参数的命令访问

--- src/executeConfig/param/index.js
+++ src/executeConfig/param/index.js
@@ -1,12 +1,34 @@
 import chalk from "chalk";
+import inquirer from "inquirer";
 import {actions} from "../../meta/constant.js";
+
+async function promptAction() {
+    const choices = Object.entries(actions).map(([key, value]) => ({
+        name: `${value} (${key})`,
+        value: key
+    }));
+
+    const { action } = await inquirer.prompt([{
+        type: 'list',
+        name: 'action',
+        message: '请选择要执行的操作:',
+        choices
+    }]);
+
+    return action;
+}
 
 export default async function param() {
     // 获取携带参数
-    const param = process.argv[2]
-    if (param === undefined || !actions.hasOwnProperty(param)) {
-        console.log(chalk.yellowBright.bold('参数无效！请检查后重新执行命令！'))
-        process.exit()
+    const paramFromCli = process.argv[2];
+    
+    // 如果没有参数或参数无效，显示交互式菜单
+    if (!paramFromCli || !actions.hasOwnProperty(paramFromCli)) {
+        if (paramFromCli) {
+            console.log(chalk.yellowBright.bold('参数无效！将显示可用选项...'));
+        }
+        return await promptAction();
     }
-    return param;
+    
+    return paramFromCli;
 }
--- src/index.js
+++ src/index.js
@@ -1,40 +1,54 @@
-import Chalk from 'chalk';
-
-import dockerConnect from './dockerService/dockerConnect.js';
+import chalk from "chalk";
 import config from './fileConfig/index.js';
 import param from './executeConfig/param/index.js';
-import deployConfig from "./executeConfig/deploy/index.js";
-import deployService from "./dockerService/deploy/index.js";
+import dockerConnect from "./dockerService/dockerConnect.js";
 import clearUnusedImages from './dockerService/clear/index.js';
 import showContainersState from './dockerService/state/index.js';
 import rollbackContainer from './dockerService/rollback/index.js';
+import deployService from "./dockerService/deploy/index.js";
+import deployConfig from "./executeConfig/deploy/index.js";
+
+async function executeAction(action, dockerInstance, configJson) {
+    switch (action) {
+        case 'clear':
+            await clearUnusedImages(dockerInstance);
+            break;
+        case 'state':
+            await showContainersState(dockerInstance);
+            break;
+        case 'rollback':
+            await rollbackContainer(dockerInstance);
+            break;
+        case 'deploy':
+            const deployConfigRes = await deployConfig(configJson);
+            if (deployConfigRes) {
+                await deployService(dockerInstance, deployConfigRes);
+            }
+            break;
+        default:
+            console.log(chalk.redBright.bold('未知的操作类型'));
+            process.exit(1);
+    }
+}
 
 export async function index() {
-    // 获取携带参数
-    const scriptParam = await param();
+    try {
+        // 获取配置文件
+        const configJson = await config();
+        
+        // 获取要执行的操作（从命令行或交互式菜单）
+        const action = await param();
+        
+        // 引导欢迎信息
+        console.log(chalk.yellowBright.bold('🌟---------------------------------------🌟\n    👏 欢迎使用自动构建部署工具 ---Maosi 👏    \n🌟---------------------------------------🌟'));
 
-    // 引导欢迎信息
-    console.log(Chalk.yellowBright.bold('🌟---------------------------------------🌟\n    👏 欢迎使用自动构建部署工具 ---Maosi 👏    \n🌟---------------------------------------🌟'));
-
-    // 读取配置文件
-    let configJson = await config();
-
-    // 链接docker
-    const dockerInstance = await dockerConnect(configJson);
-
-    // 根据参数执行清空镜像还是回滚还是部署
-    if (scriptParam === 'clear') {
-        // 清空未使用镜像
-        await clearUnusedImages(dockerInstance);
-    } else if (scriptParam === 'state') {
-        // 显示所有的容器状态
-        await showContainersState(dockerInstance);
-    } else if (scriptParam === 'rollback') {
-        // 显示容器列表，选择指定容器，再选择已存在的版本，最后确认回滚
-        await rollbackContainer(dockerInstance);
-    } else if (scriptParam === 'deploy') {
-        // 拉取镜像
-       const deployConfigRes = await deployConfig(configJson);
-       deployService(dockerInstance,deployConfigRes);
+        // 连接docker
+        const dockerInstance = await dockerConnect(configJson);
+        
+        // 执行对应的操作
+        await executeAction(action, dockerInstance, configJson);
+    } catch (error) {
+        console.error(chalk.redBright.bold('执行过程中发生错误：', error.message));
+        process.exit(1);
     }
 }
